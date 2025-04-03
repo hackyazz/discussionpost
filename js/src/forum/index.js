f5ca609c9da703c9fdcb7ef49c082b53cdb6b02f
hackyazz <yan_xz1997@163.com>
2025-04-04 00:07:07 +0800

    test simple p label

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -11,9 +11,9 @@ app.initializers.add('yazz-discussionpost', () => {
   let discussionpost = DiscussionListItem;
 
   extend(DiscussionListItem.prototype, "mainItems", function(items) {
-    let discussion = discussionpost.prototype.attrs("discussion") || {};
-    let firstPost = discussion.firstPost() || {};
-    items.add('postimage', m(PostImage, {post: firstPost}));
+    // let discussion = discussionpost.prototype.attrs("discussion") || {};
+    // let firstPost = discussion.firstPost() || {};
+    items.add('postimage', <p>discussion</p>);
   })
 
 }, -10);
