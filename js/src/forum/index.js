b9afee8febfccc409ce75e22a4f388b5820e057a
hackyazz <yan_xz1997@163.com>
2025-04-05 17:39:21 +0800

    update style

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -11,7 +11,7 @@ function extractImageSrcs(htmlString) {
   const imgElements = doc.querySelectorAll('img');
   const srcList = [];
 
-  imgElements.forEach(img => {
+  imgElements.forEach((img) => {
     if (img.src) {
       srcList.push(img.src);
     }
@@ -20,28 +20,38 @@ function extractImageSrcs(htmlString) {
   return srcList;
 }
 
-app.initializers.add('yazz-discussionpost', () => {
-  console.log('[yazz/discussionpost] Hello, forum11111!');
+app.initializers.add(
+  'yazz-discussionpost',
+  () => {
+    console.log('[yazz/discussionpost] Hello, forum11111!');
 
-  extend(DiscussionListItem.prototype, "mainItems", function(items) {
-    const discussion = this.attrs.discussion;
-    if(!discussion) {items.add("postimage", <p className='PostImage'>discussion属性不存在</p>); return;}
-    const firstPost = discussion.firstPost();
-    if(!firstPost) {items.add("postimage", <p className='PostImage'>firstpost为空</p>);return;}
-    const content = firstPost.contentHtml();
-    const imgSrcList = extractImageSrcs(content);
+    extend(DiscussionListItem.prototype, 'mainItems', function (items) {
+      const discussion = this.attrs.discussion;
+      if (!discussion) {
+        items.add('postimage', <p className="PostImage">discussion属性不存在</p>);
+        return;
+      }
+      const firstPost = discussion.firstPost();
+      if (!firstPost) {
+        items.add('postimage', <p className="PostImage">firstpost为空</p>);
+        return;
+      }
+      const content = firstPost.contentHtml();
+      const imgSrcList = extractImageSrcs(content);
 
-    if(imgSrcList.length == 0) return;
+      if (imgSrcList.length == 0) return;
 
-    items.add('postimage',
-      <p className='PostImage'>
-        {imgSrcList.map((src) => {
-          if(src.includes("assets/files/")) {
-            return <img style='width: 200px;' src={src} loading='lazy' />
-          }
-        })}
-      </p>
-    );
-  })
-
-}, -10);
+      items.add(
+        'postimage',
+        <p className="PostImageWraper">
+          {imgSrcList.map((src) => {
+            if (src.includes('assets/files/')) {
+              return <img className='PostImage' src={src} loading="lazy" />;
+            }
+          })}
+        </p>
+      );
+    });
+  },
+  -10
+);
